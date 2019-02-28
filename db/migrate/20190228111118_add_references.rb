bd593cb432a68db9505ff63e793a3b3a063e374a
Guillaume Rossi <guillaum.ro@gmail.com>
2019-02-28 12:32:44 +0100

    models done

---
+++ db/migrate/20190228111118_add_references.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class AddReferences < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :doses, :ingredient, foreign_key: true
+    add_reference :doses, :cocktail, foreign_key: true
+  end
+end
