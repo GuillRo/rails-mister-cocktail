6c059d8340e59ba7942c2972148ffd24e68c4db3
Guillaume Rossi <guillaum.ro@gmail.com>
2019-03-01 15:25:06 +0100

    finished landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 # root :to => "cocktails#index"
-
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doses, only: [:destroy]
   resources :cocktails, only: %i[index show new create] do
